af141a2846f37adbbf2424d7b06039ffd3282db6
Antonis Karamitros <ablackjim228@gmail.com>
2015-09-06 20:58:36 +0100

    update script to get today's file

--- scripts/getTodaysFile.sh
+++ scripts/getTodaysFile.sh
@@ -1,10 +1,22 @@
 
 #!/bin/zsh
+
+echo "making dir";
+mkdir -p /home/antouank/.solar/aiani/data ;
+
+echo "cd to scripts"
+cd /home/antouank/_repos_/solarytics/scripts
+
+echo "pwd:" $(pwd)
+
+echo "ftping...";
+
 TODAY=`date +%y%m%d`
 USER=$SOLAR_AIANI_USER
 PASSWD=$SOLAR_AIANI_PASSWD
 
-echo "Fetching files for " $TODAY ;
+echo --- "$(date)"
+echo "<-- Connecting to $SOLAR_AIANI_IP to fetch files for $TODAY" ;
 
 ftp -n $SOLAR_AIANI_IP <<SCRIPT
 user $USER $PASSWD
@@ -15,3 +27,14 @@ get kaco_status_$TODAY.txt
 get tag_$TODAY.txt
 quit
 SCRIPT
+
+echo "ftp script done"
+
+echo "moving files:" $(ls | grep $TODAY)
+
+mv -v $(ls | grep $TODAY) /home/antouank/.solar/aiani/data ;
+
+echo "list of /home/antouank/.solar/aiani/data" $(ls -lahG /home/antouank/.solar/aiani/data)
+
+echo 'exit code' $?
+
